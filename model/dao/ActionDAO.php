e39e26f641fb6e45345b3de7426dfb669cda14bb
Unknown <lenaerts.frederic@gmail.com>
2017-10-20 16:34:25 +0200

    refactored actionDAO

--- model/dao/ActionDAO.php
+++ model/dao/ActionDAO.php
@@ -18,62 +18,76 @@ class ActionDAO implements IActionDAO {
     }
 
     public function findAll() {
-        try {
-            $statement = $this->connection->prepare( 'SELECT * FROM actions' );
+        $query = function() {
+            $statement = $this->connection->prepare( 
+                'SELECT * 
+                FROM actions' 
+            );
+            $statement->setFetchMode( PDO::FETCH_ASSOC );
             $statement->execute();
-            $rows = $statement->fetchAll( PDO::FETCH_ASSOC );
+            return $statement->fetchAll();
+        };
 
-            $actions = array();
+        $rows = $this->tryToExecute( $query() );
 
-            for ( $i = 0; $i < count( $rows ); $i++ ) {
-                $actions[$i] = ActionFactory::CreateFromArray( $rows[$i] );
-            }
-
-            return $actions;
-        } catch ( PDOException $e ) {
-            throw new Exception( 'Caught exception: ' . $e->getMessage() );
-        } finally {
-            $this->connection = null;
+        $actions = array();
+        for ( $i = 0; $i < count( $rows ); $i++ ) {
+            $actions[$i] = ActionFactory::CreateFromArray( $rows[$i] );
         }
+
+        return $actions;
     }
 
     public function find( $id ) {
-        try {
-            $statement = $this->connection->prepare( 'SELECT * FROM actions WHERE id = :id' );
+        $query = function() use ( $id ) {
+            $statement = $this->connection->prepare( 
+                'SELECT * FROM actions 
+                WHERE id = :id' 
+            );
             $statement->setFetchMode( PDO::FETCH_ASSOC );
             $statement->bindParam( ':id', $id, PDO::PARAM_INT );
             $statement->execute();
-            $row = $statement->fetchAll();
+            return $statement->fetchAll();
+        };
 
-            if ( count( $row ) > 0 ) {
-                return ActionFactory::CreateFromArray( $row[0] );
-            } else {
-                return null;
-            }
-        } catch ( PDOException $e ) {
-            throw new Exception( 'Caught exception: ' . $e->getMessage() );
-        } finally {
-            $this->connection = null;
+        $row = $this->tryToExecute( $query() );
+
+        $action = null;
+        if ( count( $row ) > 0 ) {
+            $action = ActionFactory::CreateFromArray( $row[0] );
         }
+
+        return $action;
     }
 
     public function create( $action ) {
-        try {
-            $statement = $this->connection->prepare( 'INSERT INTO actions (action, date) VALUES (:action, :date)' );
+        $query = function() use ( $action ) {
+            $statement = $this->connection->prepare( 
+                'INSERT INTO actions (action, date) 
+                VALUES (:action, :date)' 
+            );
             $actionString = $action->getAction();
             $statement->bindParam( ':action', $actionString, PDO::PARAM_STR );
             $date = $action->getDate();
             $statement->bindParam( ':date', $date, PDO::PARAM_STR );
-            $success = $statement->execute();
+            return $statement->execute();
+        };
 
-            if ( $success ) {
-                $id = $this->connection->lastInsertId();
-                $action->setId( $id );
-                
-                return $action;
-            }
+        $hasSucceeded = $this->tryToExecute( $query() );
 
-            return null;
+        if ( $hasSucceeded ) {
+            $id = $this->connection->lastInsertId();
+            $action->setId( $id );
+        } else {
+            $action = null;
+        }
+        
+        return $action;
+    }
+
+    private function tryToExecute( $function ) {
+        try {
+            return $function;
         } catch ( PDOException $e ) {
             throw new Exception( 'Caught exception: ' . $e->getMessage() );
         } finally {
