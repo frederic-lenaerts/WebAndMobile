543a90a596af409508b89d85b1fb5c33a38a15fb
Yannick <yannick.franssen@outlook.com>
2017-11-08 18:22:27 +0100

    Include PHPUnit in the Symfony project

--- wp3/tests/AppBundle/Controller/DefaultControllerTest.php
+++ wp3/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -12,7 +12,17 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // Test for the correct HTTP response
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');        
+
+        // Test if the heading is loaded
+        $this->assertContains('Nice to meet you', $crawler->filter('h1')->text());
+        
+        // Test if location filter is loaded
+        $this->assertContains('Location', $crawler->filter('.dropdown-toggle')->text());
+        $this->assertContains('London', $crawler->filter('.dropdown-item')->text());
+
+        // Test whether the heading of the table is being generated
+        $this->assertContains('#', $crawler->filter('th')->text());
     }
 }
