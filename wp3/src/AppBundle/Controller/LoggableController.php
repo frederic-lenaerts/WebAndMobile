e567bc99bd99c35745f7da6de7402ad3d756946c
Unknown <lenaerts.frederic@gmail.com>
2017-11-17 10:21:16 +0100

    refactor logging

--- wp3/src/AppBundle/Controller/LoggableController.php
+++ wp3/src/AppBundle/Controller/LoggableController.php
@@ -9,10 +9,15 @@ abstract class LoggableController extends Controller {
 
     protected function logUserVisitAt( $route ) {
         $logger = $this->get('monolog.logger.user_activity');       
+        $user = $this->getUsername();
+        $logger->info( $user.' visited '.$route.'.' );
+    }
+
+    private function getUsername() {
         $user = $this->get( 'security.token_storage' )->getToken()->getUser();
         if ( !is_string( $user )) {
             $user = $user->getUsername();
         }
-        $logger->info( $user.' visited '.$route.'.' );
+        return $user;
     }
 }
